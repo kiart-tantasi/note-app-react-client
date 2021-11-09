75c977500db4da4ada8c080f2bd0d4868c099ea2
kiart-tantasi <petch_rwb@hotmail.com>
2021-11-09 20:27:11 +0700

    Note App and deleted useRef

--- src/AddItem.js
+++ src/AddItem.js
@@ -1,13 +1,8 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useState } from 'react';
 import AddIcon from '@mui/icons-material/Add';
 
 export default function AddItem(props) {
   const [input,setInput] = useState({item:"",des:""});
-  const itemRef = useRef();
-
-  useEffect(()=> {
-    itemRef.current.focus();
-  })
 
   function handleInputChange(e) {
     const {name,value} = e.target;
@@ -22,8 +17,7 @@ export default function AddItem(props) {
       <form>
       <h3>Add Item</h3>
         <div className="Two-input">
-          {/* ref={props.inputRef} */}
-          <input ref={itemRef} onChange={handleInputChange} value={input.item} type="text" name="item" id="First-input" placeholder="Title"></input>
+          <input onChange={handleInputChange} value={input.item} type="text" name="item" id="First-input" placeholder="Title"></input>
           <input onChange={handleInputChange} value={input.des} type="text" name="des" placeholder="Description"></input>
         </div>
         <button onClick={(e) => {
