9536b36672526c82fbf06a677b9d2b65bfc5ff5f
kiart-tantasi <petch_rwb@hotmail.com>
2022-01-04 22:27:08 -0800

    separate fetchData function into another file

--- src/shared/PostContext.js
+++ src/shared/PostContext.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import fetchData from "./fetchData";
 import generateId from "./generateId";
 
 const PostContext = React.createContext({});
@@ -11,50 +12,15 @@ function PostContextProvider(props) {
 
   // -------------------- USE EFFECT -------------------- //
   useEffect(() => {
-    async function loadData() {
-      setIsLoading(true);
-      let initialPosts;
-      // Trying getting data from server first if authenticated
-      try {
-        const response = await fetch("/api/user",{credentials:"include"});
-        if (response.ok) {
-          const userData = await response.json();
-          initialPosts = userData.posts;
-          setUserName(userData.userName);
-          setIsLoggedIn(true);
-        } else {
-          throw new Error("offline mode activated")
-        }
-      // If not authenticated then get data from local storage or just use default posts
-      } catch (err) {
-        console.log(err.message);
-        if (localStorage.getItem("myPostIt")) {
-          initialPosts = JSON.parse(localStorage.getItem("myPostIt"));
-          // if never updated... 1.0.1
-          if (!localStorage.getItem("myPostItUpdate")) {
-            const reversedArray = [...initialPosts].reverse();
-            localStorage.setItem("myPostIt", JSON.stringify(reversedArray));
-            initialPosts = reversedArray;
-            localStorage.setItem("myPostItUpdate", JSON.stringify({reversedArray: true}));
-          }
-        } else {
-          initialPosts = [
-            {_id: generateId(), item: "โพสต์แรกของฉัน", des:"ดูแลสุขภาพด้วยงับ", date: new Date().getTime()},
-            {_id: generateId(), item: "ประชุมเช้า", des:"วันพุธ เข้าzoomก่อน 10 โมง", date: new Date().getTime()},
-            {_id: generateId(), item: "จันทร์หน้า", des:"ทำ OT ...", date: new Date().getTime()},
-            {_id: generateId(), item: "หยุดปีใหม่", des:"วันศุกร์หน้าแล้ว!", date: new Date().getTime()}
-          ];
-          localStorage.setItem("myPostIt", JSON.stringify(initialPosts));
-          localStorage.setItem("myPostItUpdate", JSON.stringify({reversedArray: true}));
-        }
-        setIsLoggedIn(false);
-      }
-      setPosts(initialPosts);
-      setIsLoading(false);
+    setIsLoading(true);
+    async function fetchDataFunction() {
+      const data = await fetchData();
+      setPosts(data.posts);
+      setIsLoggedIn(data.isLoggedIn);
+      setUserName(data.userName);
     }
-
-    loadData();
-
+    fetchDataFunction();
+    setIsLoading(false);
   }, [isLoggedIn] );
   // -------------------- END OF useEffect --------------------//
   
---
+++ src/shared/fetchData.js
@@ -0,0 +1,88 @@
+import generateId from "./generateId";
+
+export default async function fetchData() {
+    let initialPosts;
+    let userName;
+    let isLoggedIn;
+    try {
+      const response = await fetch("/api/user",{credentials:"include"});
+      if (response.ok) {
+        const userData = await response.json();
+        initialPosts = userData.posts;
+        userName = userData.userName;
+        isLoggedIn = true;
+      } else {
+        throw new Error("offline mode activated")
+      }
+    } catch (err) {
+      console.log(err.message);
+      if (localStorage.getItem("myPostIt")) {
+        const retrievedPosts = JSON.parse(localStorage.getItem("myPostIt"));
+        if (!localStorage.getItem("myPostItUpdate")) {
+          const reversedArray = [...retrievedPosts].reverse();
+          localStorage.setItem("myPostIt", JSON.stringify(reversedArray));
+          initialPosts = reversedArray;
+          localStorage.setItem("myPostItUpdate", JSON.stringify({reversedArray: true}));
+        } else {
+            initialPosts = retrievedPosts;
+        }
+      } else {
+        initialPosts = [
+          {_id: generateId(), item: "โพสต์แรกของฉัน", des:"ดูแลสุขภาพด้วยงับ", date: new Date().getTime()},
+          {_id: generateId(), item: "ประชุมเช้า", des:"วันพุธ เข้าzoomก่อน 10 โมง", date: new Date().getTime()},
+          {_id: generateId(), item: "จันทร์หน้า", des:"ทำ OT ...", date: new Date().getTime()},
+          {_id: generateId(), item: "หยุดปีใหม่", des:"วันศุกร์หน้าแล้ว!", date: new Date().getTime()}
+        ];
+        localStorage.setItem("myPostIt", JSON.stringify(initialPosts));
+        localStorage.setItem("myPostItUpdate", JSON.stringify({reversedArray: true}));
+      }
+      isLoggedIn = false;
+    }
+    return {
+        posts: initialPosts,
+        isLoggedIn: isLoggedIn,
+        userName: userName
+    } 
+}
+
+// async function loadData() {
+//     let initialPosts;
+//     // Trying getting data from server first if authenticated
+//     try {
+//         const response = await fetch("/api/user",{credentials:"include"});
+//         if (response.ok) {
+//             const userData = await response.json();
+//             initialPosts = userData.posts;
+//             setUserName(userData.userName);
+//             setIsLoggedIn(true);
+//         } else {
+//             throw new Error("offline mode activated")
+//         }
+//     // If not authenticated then get data from local storage or just use default posts
+//     } catch (err) {
+//         console.log(err.message);
+//         if (localStorage.getItem("myPostIt")) {
+//             const retrievedPosts = JSON.parse(localStorage.getItem("myPostIt"));
+//             // if never updated... 1.0.1
+//             if (!localStorage.getItem("myPostItUpdate")) {
+//                 const reversedArray = [...retrievedPosts].reverse();
+//                 localStorage.setItem("myPostIt", JSON.stringify(reversedArray));
+//                 initialPosts = reversedArray;
+//                 localStorage.setItem("myPostItUpdate", JSON.stringify({reversedArray: true}));
+//             } else {
+//                 initialPosts = retrievedPosts;
+//             }
+//         } else {
+//             initialPosts = [
+//                 {_id: generateId(), item: "โพสต์แรกของฉัน", des:"ดูแลสุขภาพด้วยงับ", date: new Date().getTime()},
+//                 {_id: generateId(), item: "ประชุมเช้า", des:"วันพุธ เข้าzoomก่อน 10 โมง", date: new Date().getTime()},
+//                 {_id: generateId(), item: "จันทร์หน้า", des:"ทำ OT ...", date: new Date().getTime()},
+//                 {_id: generateId(), item: "หยุดปีใหม่", des:"วันศุกร์หน้าแล้ว!", date: new Date().getTime()}
+//             ];
+//             localStorage.setItem("myPostIt", JSON.stringify(initialPosts));
+//             localStorage.setItem("myPostItUpdate", JSON.stringify({reversedArray: true}));
+//         }
+//         setIsLoggedIn(false);
+//     }
+//     setPosts(initialPosts);
+// }
