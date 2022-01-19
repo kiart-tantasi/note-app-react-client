0ce122864ca6a23120cb3cfdad8c6e53d32b4dc6
kiart-tantasi <petch_rwb@hotmail.com>
2022-01-19 08:09:02 +0700

    2022 copyright

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -4,7 +4,7 @@ import styles from "./Footer.module.css";
 export default function Footer() {
     return (
         <footer className={styles.footer}>
-            <p>Copyright 2021 © created and designed by Kiart Tantasi</p>
+            <p>Copyright 2022 © created and designed by Kiart Tantasi</p>
         </footer>
     )
 }
